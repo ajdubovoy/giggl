f57711ffcd4f9b175051559881860ff03ef01289
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-21 18:09:01 +0200

    incorporate first draft of sender/receiver logic based on current user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  private
+
+  def sender
+    player = booking.band
+    organizer = booking.gig.organizer
+
+    if current_user.bands.include? player
+      return player
+    else
+      return organizer
+    end
+  end
+
+  def receiver
+    player = booking.band
+    organizer = booking.gig.organizer
+
+    if current_user.venues.include? organizer || current_user.bands.include? organizer
+      return organizer
+    else
+      return player
+    end
+  end
 end
