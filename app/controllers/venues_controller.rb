92a1fc8dd559de03457d9c2dd4949bc8b5580840
iain russell <iainrussell89@gmail.com>
2018-08-21 17:11:09 +0200

    Made form_for for forms

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,25 @@
 class VenuesController < ApplicationController
+  def index 
+    @venues = Venue.all
+  end 
+
+  def show
+    @venue = Venue.find(params[:id])
+  end 
+
+  def new 
+    @venue = Venue.new
+    @venue.user = current_user 
+  end 
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user = current_user
+    @venue.save
+    redirect_to venue_path(@venue)
+  end 
+
+  def venue_params
+    params.require(:venue).permit(:name, :telephone, :description)
+  end
 end
