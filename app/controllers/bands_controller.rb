e2fbaf1a3b2fcdd56a0b6591d229450a543f6eea
Emily Rhodes <emilyerhodes@hotmail.com>
2018-08-21 16:36:21 +0200

    bands controller yooooo

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,28 @@
 class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    @band.user = User.find(band_params[:user_id])
+    if @band.save
+      redirect_to band_path(@band)
+    else
+      render :new
+    end
+  end
+
+  private
+  def band_params
+    params.permit(:band).require(:bio, :website, :soundcloud, :spotify, :bandcamp, :facebook, :instagram, :twitter, :youtube, :vimeo, :name, :user_id)
+  end
 end
