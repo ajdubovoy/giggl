d3be822820b5c8a811a8598d18ca8b931226f11e
Emily Rhodes <emilyerhodes@hotmail.com>
2018-08-21 17:03:12 +0200

    venue controllzz

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,28 @@
 class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+    @venues = Venue.find(params[:id])
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user = current_user
+    if @venue.save
+      redirect_to venue_path(@venue)
+    else
+      render :new
+    end
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:address, :telephone, :email, :description, :website, :name)
+  end
 end
