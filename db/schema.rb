f4ec61286ec4e71bba798e7766175f2332d4379b
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-21 11:11:00 +0200

    generate model Genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_090515) do
+ActiveRecord::Schema.define(version: 2018_08_21_091028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2018_08_21_090515) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_bands_on_user_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.bigint "band_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_genres_on_band_id"
   end
 
   create_table "photos", force: :cascade do |t|
@@ -66,5 +74,6 @@ ActiveRecord::Schema.define(version: 2018_08_21_090515) do
   end
 
   add_foreign_key "bands", "users"
+  add_foreign_key "genres", "bands"
   add_foreign_key "venues", "users"
 end
