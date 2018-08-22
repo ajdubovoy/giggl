90c39a80eda66033acfa6b692e33258917711520
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-22 16:26:52 +0200

    code index and show actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,12 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
     @booking = Booking.new(booking_params)
     @booking.band = band
