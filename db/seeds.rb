aa05d0a599f57d61b1dde50183c3e7eecbcc287a
Emily Rhodes <emilyerhodes@hotmail.com>
2018-08-22 16:36:50 +0200

    seeds should make some sense now

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@
   Band.destroy_all
   Genre.destroy_all
   User.destroy_all
+  Gig.destroy_all
 
 rock = Genre.create(name:"rock")
 funk = Genre.create(name:"funk")
@@ -23,16 +24,16 @@ user3 = User.create(email: "oriane@oriane.com", password: "123456789", name: "Or
 user4 = User.create(email: "alex@alex.com", password: "123456789", name: "Alexander Dubovoy")
 users = User.all
 
-band1 = Band.create(bio: "A combination of soul, funk, and alternative stylings, embedded in contemporary experimentation", website: "www.tattatband.com", spotify: "www.spotify.com", bandcamp: "www.bandcamp.com", facebook: "www.facebook.com/tattatband", instagram: "https://www.instagram.com/tattat_band/?hl=en", twitter: "www.twitter.com", youtube: "www.youtube.com", vimeo: "www.vimeo.com", name: "TATTAT", address: "Berlin")
+band1 = Band.create(bio: "A combination of soul, funk, and alternative stylings, embedded in contemporary experimentation", website: "www.tattatband.com", spotify: "www.spotify.com", bandcamp: "www.bandcamp.com", facebook: "www.facebook.com/tattatband", instagram: "https://www.instagram.com/tattat_band/?hl=en", twitter: "www.twitter.com", youtube: "www.youtube.com", vimeo: "www.vimeo.com", name: "Mistar Anderson", address: "Berlin")
 band1.genre = genres.sample
 band1.user = users.sample
 band2 = Band.create(bio: "A combination of soul, funk, and alternative stylings, embedded in contemporary experimentation", website: "www.tattatband.com", spotify: "www.spotify.com", bandcamp: "www.bandcamp.com", facebook: "www.facebook.com/tattatband", instagram: "https://www.instagram.com/tattat_band/?hl=en", twitter: "www.twitter.com", youtube: "www.youtube.com", vimeo: "www.vimeo.com", name: "TATTAT", address: "Columbus, Ohio")
 band2.genre = genres.sample
 band2.user = users.sample
-band3 = Band.create(bio: "A combination of soul, funk, and alternative stylings, embedded in contemporary experimentation", website: "www.tattatband.com", spotify: "www.spotify.com", bandcamp: "www.bandcamp.com", facebook: "www.facebook.com/tattatband", instagram: "https://www.instagram.com/tattat_band/?hl=en", twitter: "www.twitter.com", youtube: "www.youtube.com", vimeo: "www.vimeo.com", name: "TATTAT", address: "Berlin")
+band3 = Band.create(bio: "A combination of soul, funk, and alternative stylings, embedded in contemporary experimentation", website: "www.tattatband.com", spotify: "www.spotify.com", bandcamp: "www.bandcamp.com", facebook: "www.facebook.com/tattatband", instagram: "https://www.instagram.com/tattat_band/?hl=en", twitter: "www.twitter.com", youtube: "www.youtube.com", vimeo: "www.vimeo.com", name: "Booty & the kidd", address: "Berlin")
 band3.genre = genres.sample
 band3.user = users.sample
-band4 = Band.create(bio: "A combination of soul, funk, and alternative stylings, embedded in contemporary experimentation", website: "www.tattatband.com", spotify: "www.spotify.com", bandcamp: "www.bandcamp.com", facebook: "www.facebook.com/tattatband", instagram: "https://www.instagram.com/tattat_band/?hl=en", twitter: "www.twitter.com", youtube: "www.youtube.com", vimeo: "www.vimeo.com", name: "TATTAT", address: "Berlin")
+band4 = Band.create(bio: "A combination of soul, funk, and alternative stylings, embedded in contemporary experimentation", website: "www.tattatband.com", spotify: "www.spotify.com", bandcamp: "www.bandcamp.com", facebook: "www.facebook.com/tattatband", instagram: "https://www.instagram.com/tattat_band/?hl=en", twitter: "www.twitter.com", youtube: "www.youtube.com", vimeo: "www.vimeo.com", name: "Stephen King", address: "Berlin")
 band4.genre = genres.sample
 band4.user = users.sample
 bands = Band.all
@@ -42,6 +43,21 @@ venue2 = Venue.create(name: "Taranino's Bar", address: "Brunnenstraße 163, Berl
 venue3 = Venue.create(name: "Le Titty Twister", address: "5 Rue de Berri, 75008 Paris", telephone: "+33 1 45 62 50 23", email: "titties@doubleD.com", description: "sun's out", website: "http://www.letittytwister.com", user: User.all.sample)
 venue4 = Venue.create(name: "Donau115", address: "Donaustraße 115, 12043 Berlin", telephone: "+49 0179 1809168", email: "alex@rules.com", description: "the gitmeister played there", website: "http://donau115.de", user: User.all.sample)
 venues = Venue.all
+
+gig1 = Gig.create(address: "London", paid: true, description: "Tuesdays at Dirty Dicks is bringing you live music - to our new Side Stage. We are also featuring $1 off Pacifico, Modelo, and Ballast Point!", other_bands: "Mistar Anderson, Josh Hill", url: "https://www.facebook.com/events/1109582902525113/", name: "Tuesdays @ Dirty Dicks", date: '09/09/18', time: '20:30')
+gig1.genre = genres.sample
+gig1.organizer = venue1
+gig2 = Gig.create(address: "Berlin", paid: true, description: "We need an opener for our EP release show", other_bands: "TATTAT", url: "https://www.facebook.com/events/1109582902525113/", name: "TATTAT EP release show", date: '09/09/18', time: '20:30')
+gig2.genre = genres.sample
+gig2.organizer = band1
+gig3 = Gig.create(address: "Donaustraße 115, 12043 Berlin", paid: true, description: "Live music act wanted saturday pm", other_bands: "Mistar Anderson, Josh Hill", url: "https://www.facebook.com/events/1109582902525113/", name: "Tuesdays @ Dirty Dicks", date: '09/09/18', time: '20:30')
+gig3.genre = genres.sample
+gig3.organizer = venue4
+gig4 = Gig.create(address: "Columbus", paid: false, description: "Come play a house show with us", other_bands: "Mistar Anderson, Josh Hill", url: "https://www.facebook.com/events/1109582902525113/", name: "House show @ Misfit Manor", date: '09/09/18', time: '20:30')
+gig4.genre = genres.sample
+gig4.organizer = bands.sample
+gigs = Gig.all
+
 
 
 
