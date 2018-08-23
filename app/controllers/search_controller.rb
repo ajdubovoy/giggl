5dd36b4f1ad40c5b4cc13377e58e5bfbba7a2fc9
iain russell <iainrussell89@gmail.com>
2018-08-23 18:19:11 +0200

    merge

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,9 @@
 class SearchController < ApplicationController
   def index
     @gigs = Gig.all
-
     # Location filter
     if params[:location].present?
-      @gigs = Gig.near(params[:location]
+      @gigs = Gig.near(params[:location])
     end
   end
 end
