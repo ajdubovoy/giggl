628fcc0af4d71f3d01e7e949c28c6daadbf1c730
Emily Rhodes <emilyerhodes@hotmail.com>
2018-08-24 12:05:12 +0200

    photo things

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,6 +2,7 @@ class Gig < ApplicationRecord
   belongs_to :genre
   belongs_to :organizer, polymorphic: true
   has_many :bands, through: :bookings
+  has_many :photos, as: :profile
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
