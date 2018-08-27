58a2d83eddf41a52f33cc0278eaf7d2a1ee3764a
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-27 16:35:31 +0200

    fix

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,6 +1,7 @@
 class Gig < ApplicationRecord
   belongs_to :genre
   belongs_to :organizer, polymorphic: true
+  has_many :bookings
   has_many :bands, through: :bookings
   has_many :photos, as: :profile
   geocoded_by :address
