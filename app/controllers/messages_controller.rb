e128f7cabe5a0f39d58b4f2cc53475d50bb7ec06
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-28 11:59:39 +0200

    messages work

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,17 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
  def new
-  @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
-end
+    @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
+  end
 
   def create
-    gig = Gig.find(params[:gig_id])
-    booking = Booking.create(gig: gig, band: current_user.bands.first)
     recipients = User.where(id: params['recipients'])
-    conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
+    @conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
+    @gig = Gig.find(params[:gig_id])
+    @booking = Booking.create(gig: @gig, status: "pending", band: current_user.bands.first)
+    @conversation.booking_id = @booking.id
+    @conversation.save
     flash[:success] = "Message has been sent!"
-    redirect_to conversation_path(conversation)
+    redirect_to conversation_path(@conversation)
   end
 end
